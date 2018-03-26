1ee9b9972040da4247c09f9e60ddbc7bc3612c22
roshnivp <roshniknambiar@gmail.com>
2018-03-25 20:29:18 -0700

    Revision

--- src/PracticeQstns/ShortestArrayWindow.java
+++ src/PracticeQstns/ShortestArrayWindow.java
@@ -12,52 +12,36 @@ public class ShortestArrayWindow {
     static int solution(int[] A) {  List<Integer> minWindow = new ArrayList<>();
         int[] ans;
         HashMap<Integer, Integer> unique = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < A.length; i++) {
-            if(!(list.contains(A[i]))){
-                list.add(A[i]);
-            }
-        }
-        for (int i = 0; i < list.size(); i++) {
-            int c = list.get(i);
+            int c = A[i];
             if (!unique.containsKey(c))
                 unique.put(c, 1);
-            else
-                unique.put(c, unique.get(c) + 1);
         }
+
         HashMap<Integer, Integer> found = new HashMap<>();
         int foundCounter = 0;
         int start = 0;
         int end = 0;
         int min = Integer.MAX_VALUE;
 
-        //
         while (end < A.length) {
-            int c = A[end];
-            if (unique.containsKey(c)) {
+                int c = A[end];
                 if (found.containsKey(c)) {
-                    if (found.get(c) < unique.get(c))
-                        foundCounter++;
                     found.put(c, found.get(c) + 1);
-                } else {
+                }
+                else {
                     found.put(c, 1);
                     foundCounter++;
                 }
-            }
-            if (foundCounter == list.size()) {
-                //When foundCounter equals to T.length(), in other words, found all characters.
+
+            if (foundCounter == unique.size()) {
                 int sc = A[start];
-                while (!found.containsKey(sc) || found.get(sc) > unique.get(sc)) {
-                    if (found.containsKey(sc) && found.get(sc) > unique.get(sc))
-                        found.put(sc, found.get(sc) - 1);
+                while (found.get(sc) > unique.get(sc)) {
+                    found.put(sc, found.get(sc) - 1);
                     start++;
                     sc = A[start];
                 }
                 if (end - start + 1 < min) {
-                    //int length = end-start;
-                   // ans = new int[length];
-                   // ans = Arrays.copyOfRange(A, start, end + 1);
-                    // minWindow = Arrays.asList(ans);
                     min = end - start + 1;
                 }
             }
